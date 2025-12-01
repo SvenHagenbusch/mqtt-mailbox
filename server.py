1a3e80f2c3884c184f15af570f5142807bf92337
heofthetea <heofthetea@gmail.com>
2025-12-01 09:29:40 +0100

    remove remaining script from index.html

--- server.py
+++ server.py
@@ -8,6 +8,7 @@ import json
 import uvicorn
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.requests import Request
 from amqtt.client import MQTTClient
@@ -30,6 +31,7 @@ logger = logging.getLogger("Server")
 
 # --- FastAPI Setup ---
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
